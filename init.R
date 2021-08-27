bbf8b8aaa1c1dc753c26b4d482f7c3c42aa2a50a
votieuvu1986 <86217523+votieuvu1986@users.noreply.github.com>
2021-08-28 02:09:59 +0700

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,30 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("randomForest", "data.table")
+my_packages = c("shiny", 
+                "shinyBS",
+               "shinythemes",
+               "shinyjs",
+               "shinyWidgets",
+               "shinydashboard",
+               "shinycssloaders",
+               "tidyverse",
+               "DT",
+               "leaflet",
+               "tibble",
+               "sp",
+               "lubridate",
+               "rmarkdown",
+               "flextable",
+               "webshot",
+               "rintrojs",
+               "feather",
+               "readxl",
+               "xts",
+               "plotly",
+               "dygraphs",
+               "tsfknn",
+               "forecast")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
